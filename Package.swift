0a1946af2fc681e9042f18a429ad3b756c55450d
Alexandr Zhuchinskiy <oleksandr.zhuchinskiy@taboola.com>
2022-10-11 16:44:22 +0300

    SwiftUI Umbrella Framework 3.8.3 release
    - update TaboolaSDK to the 3.8.3 version.

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "TaboolaSDK",
-            url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/spm/3.7.0/TaboolaSDK.xcframework.zip",
-            checksum: "3e1c404618758a13e64f998a71802f110eb4b14f08ec3fca67f29b4142b3a2e2"
+            url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/spm/3.8.3/TaboolaSDK.xcframework.zip",
+            checksum: "b9bff93c39d98c8fd8d61629755117302d372931840289dd9214ae348dba89bd"
         )
     ]
 )
